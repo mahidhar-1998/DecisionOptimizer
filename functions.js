f3f45687bc71265ee8205d205340635b2df0db71
smucharlainfoane <smucharla@infoane.com>
2023-10-17 17:07:04 +0530

    Included  removeURLparameter function

--- functions.js
+++ functions.js
@@ -7,3 +7,20 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+function removeURLParameter(url, parameter) {
+	var urlParts = url.split('?');
+	if (urlParts.length >= 2) {
+		var params = urlParts[1].split('&');
+		var newParams = [];
+		for (var i = 0; i < params.length; i++) {
+			var parameterName = params[i].split('=')[0];
+			if (parameterName !== parameter) {
+				newParams.push(params[i]);
+			}
+		}
+		return urlParts[0] + (newParams.length > 0 ? '?' + newParams.join('&') : '');
+	}
+	return url;
+}
+
